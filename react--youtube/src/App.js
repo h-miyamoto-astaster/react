dce69a56cf173e7a9a878796ec6cf619d9d723d0
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-06-27 14:20:00 +0900

    動画30分ぐらいまで

---
+++ react--youtube/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos,setTodos] = useState(["Todo1","Todo2"]);
+  
+  return (
+    <>
+      <TodoList todos={todos}/>
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
+  );
+}
+
+export default App;
