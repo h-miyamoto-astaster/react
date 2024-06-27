4550a3c846da02ec51ad571749820db80bb6f64a
ebans1 <ebans@outlook.jp>
2024-06-27 23:20:23 +0900

    37分ぐらいまで

--- react--youtube/src/App.js
+++ react--youtube/src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos,setTodos] = useState(["Todo1","Todo2"]);
+  const [todos,setTodos] = useState([
+    {id:1,name:"Todo1",completed:false}
+  ]);
   
+const todoNameRef = useRef()
+
+const handleAddTodo = (e) => {
+  //タスクを追加する
+  console.log(todoNameRef.current.value);
+}
+
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>タスクを追加</button>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
     </>
