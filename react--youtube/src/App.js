fb1c285db8e007e3453ac0a54b83ab178d6406cf
ebans1 <ebans@outlook.jp>
2024-07-03 23:50:39 +0900

    バリデーション動画25分ぐらいまで

--- react--youtube/src/App.js
+++ react--youtube/src/App.js
@@ -1,22 +1,37 @@
+import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const initialValues = {userName :"",mailAddress:"",password:""};
+  const [formValues,setFormValues] = useState(initialValues);
 
-  const [count,setCount] = useState(0);
-
-  const handleCount = (num) =>{
-    setCount((current) =>{
-      return current + num;
-    })
+  const handleChange = (e) =>{
+    const {name,value} = e.target;
+    setFormValues({...formValues,name:value})
   }
+
   return (
-    <>
-    <div className="App">
-      <button onClick={() =>handleCount(-1)}>-</button>
-      <button onClick={() =>handleCount(+1)}>+</button>
-      <p>{count}</p>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input type="text" placeholder="ユーザー名" name="username" onChange={(e) => handleChange(e) }/>
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input type="text" placeholder="メールアドレス" name="mailAddress"/>
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder="パスワード" name="password"/>
+          </div>
+          <button className="submitButton">ログイン</button>
+        </div>
+      </form>
     </div>
-    </>
   );
 }
 
