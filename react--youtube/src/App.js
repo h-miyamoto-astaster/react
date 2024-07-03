3122be2fd960f05c1dfe49643725c959ee8e1eda
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-07-03 12:00:59 +0900

    動画ひとまず終了

--- react--youtube/src/App.js
+++ react--youtube/src/App.js
@@ -1,15 +1,18 @@
-import { useEffect,useState } from "react";
+import { useEffect,useRef,useState } from "react";
 
 function App() {
-  const [textState,setTextState] = useState("");
+  //const [textState,setTextState] = useState("");
+  const textRef = useRef();
+  const [flag,setFlag] = useState(false);
   useEffect(() =>{
     console.log("副作用");
-  },[]);
+  },[flag]);
   console.log("レンダリング")
 
   return (
     <div className="App">
-      <input type="text" value={textState} onChange = {(e) => setTextState(e.target.value)} />
+      <input type="text" ref={textRef} />
+      <button onClick={()=>setFlag((example) => !example)}>送信</button>
     </div>
   );
 }
