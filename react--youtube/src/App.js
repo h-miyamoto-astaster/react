402ed1a8ee6c1c8273fd3754fde0d6a5cf4eb456
ebans1 <ebans@outlook.jp>
2024-07-12 23:52:45 +0900

    動画一旦終了

--- react--youtube/src/App.js
+++ react--youtube/src/App.js
@@ -5,13 +5,19 @@ import {useState,useEffect} from "react"
 function App() {
   const [name,setName] = useState('');
   const [id,setId] = useState('deatiger');
+  const ids = ['deatiger','gaearon','aws','google','facebook'];
+
+  const getRandomId = () =>{
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id);
+  }
   
   useEffect(() =>{
-    fetch(`https://api.github.com/users/{id}`)
+    fetch(`https://api.github.com/users/${id}`)
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      //setName(data.name)
+      setName(data.name)
     })
     .catch(error =>{
       console.log(error)
@@ -21,7 +27,7 @@ function App() {
   return(
     <>
       <p>{id}の、github上の名前は{name}です。</p>
-      {/*<button onClick={getRandomId}>IDを変更</button>*/}
+      <button onClick={getRandomId}>IDを変更</button>
     </>
   )
 }
