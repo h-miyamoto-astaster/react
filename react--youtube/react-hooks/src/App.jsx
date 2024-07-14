3a2033310e48e6704f62ec8a0b3f0c8437b27059
ebans1 <ebans@outlook.jp>
2024-07-15 00:19:07 +0900

    動画21分ぐらいまで終了

--- react--youtube/react-hooks/src/App.jsx
+++ react--youtube/react-hooks/src/App.jsx
@@ -1,9 +1,15 @@
-import {useState} from "react";
+import {useEffect, useState,useContext} from "react";
 import './App.css'
+import ShinCodeContext from "./main";
 
 function App() {
   const [count,setCount] = useState(0)
+  const shinCodeInfo = useContext(ShinCodeContext);
   
+  useEffect(() => {
+    console.log("hello.hooks");
+  },[])
+
   const handleClick = () =>{
     setCount(count + 1)
   }
@@ -12,6 +18,11 @@ function App() {
       <h1>useState</h1>
       <button onClick={handleClick}>+</button>
       <p>{count}</p>
+
+      <hr />
+      <h1>useContext</h1>
+      <p>{shinCodeInfo.name}</p>
+      <p>{shinCodeInfo.age}</p>
     </>
   )
 }
--- react--youtube/react-hooks/src/main.jsx
+++ react--youtube/react-hooks/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const shinCodeInfo = {
+  name:"shinCode",
+  age:24
+}
+
+const shinCodeContext = createContext(shinCodeInfo)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <shinCodeContext value={shinCodeInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </shinCodeContext>
 )
+
+export default shinCodeContext
