eb0f8414f6bb3c4d4b65abf41c7e6950d61acc8f
ebans1 <ebans@outlook.jp>
2024-07-16 23:18:41 +0900

    動画46分ぐらいまで終了

--- react--youtube/react-hooks/src/App.jsx
+++ react--youtube/react-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState,useContext, useRef} from "react";
+import {useEffect, useState,useContext, useRef,useMemo} from "react";
 import './App.css'
 import ShinCodeContext from "./main";
 
@@ -31,6 +31,14 @@ function App() {
     return count02 * count02;
   }*/
 
+  const square = useMemo(() =>{
+    let i= 0;
+    while( i < 200000){
+      i++;
+    }
+    return count02 * count02;
+  },[count02])
+
   return (
     <>
       <h1>useState</h1>
@@ -49,7 +57,7 @@ function App() {
       <h1>useMemo</h1>
       <div>カウント１:{count01}</div>
       <div>カウント２:{count01}</div>
-      <div>結果{square()}</div>
+      <div>結果{square}</div>
       <button onClick={() => setCount01(count01 + 1)}>＋</button>
       <button onClick={() => setCount02(count02 + 1)}>＋</button>
     </>
