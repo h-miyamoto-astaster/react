890f7e52987547d0212b409d9da5feefdedf1ec9
ebans1 <ebans@outlook.jp>
2024-07-16 23:48:37 +0900

    本203ページまで終了

--- react--youtube/react-hooks/src/App.jsx
+++ react--youtube/react-hooks/src/App.jsx
@@ -1,65 +1,49 @@
 import {useEffect, useState,useContext, useRef,useMemo} from "react";
 import './App.css'
-import ShinCodeContext from "./main";
 
 function App() {
-  const [count,setCount] = useState(0)
-  const shinCodeInfo = useContext(ShinCodeContext);
-  const ref = useRef();
+  const INITIAL_COUNT = 0;
   
-  useEffect(() => {
-    console.log("hello.hooks");
-  },[])
+  const INITIAL_NAME = "JavaScript";
 
-  const handleClick = () =>{
-    setCount(count + 1)
-  }
+  const sampleComponent = () => {
+    const [count,setCount] = useState(INITIAL_COUNT);
 
-  const handleRef = () =>{
-    console.log(ref.current.value);
-    console.log(ref.current.offsetHeight);
-  }
+    const [name,setNAME] = useState(INITIAL_NAME);
 
-  const [count01,setCount01] = useState(0);
-  const [count02,setCount02] = useState(0);
+    const countIncrement = () => setCount((prevCount) => prevCount + 1);
 
-  /*const square = () =>{
-    let i= 0;
-    while( i < 2000000){
-      i++;
-    }
-    return count02 * count02;
-  }*/
+    const countDecrement = () => setCount((prevCount) => prevCount - 1);
 
-  const square = useMemo(() =>{
-    let i= 0;
-    while( i < 200000){
-      i++;
-    }
-    return count02 * count02;
-  },[count02])
+    const countReset = () => setCount(INITIAL_COUNT);
+
+    const handleChangeName = (e) =>{
+      setNAME(e.target.value);
+    } ;
+
+    return(
+      <div className="App">
+        <p>
+          現在のカウント数:<b>{count}</b>
+          <br />
+          countの初期値:<b>{INITIAL_COUNT}</b>
+        </p>
+        <button onClick={countIncrement}>increment</button>
+        <button onClick={countDecrement}>decrement</button>
+        <button onClick={countReset}>reset</button>
+        <p>
+        Hello,<b>{name}!!</b>
+        <br />
+        nameの初期値:<b>{INITIAL_NAME}</b>
+        </p>
+        <input type="text" onChange={handleChangeName} />
+      </div>
+    );
+  };
 
   return (
     <>
-      <h1>useState</h1>
-      <button onClick={handleClick}>+</button>
-      <p>{count}</p>
 
-      <hr />
-      <h1>useContext</h1>
-      <p>{shinCodeInfo.name}</p>
-      <p>{shinCodeInfo.age}</p>
-
-      <h1>useRef</h1>
-      <input type="text" ref={ref}/>
-      <button onClick={handleRef}>useRef</button>
-
-      <h1>useMemo</h1>
-      <div>カウント１:{count01}</div>
-      <div>カウント２:{count01}</div>
-      <div>結果{square}</div>
-      <button onClick={() => setCount01(count01 + 1)}>＋</button>
-      <button onClick={() => setCount02(count02 + 1)}>＋</button>
     </>
   )
 }
--- react--youtube/react-hooks/src/main.jsx
+++ react--youtube/react-hooks/src/main.jsx
@@ -1,21 +1,10 @@
-import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const shinCodeInfo = {
-  name:"shinCode",
-  age:24
-}
-
-const shinCodeContext = createContext(shinCodeInfo)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <shinCodeContext value={shinCodeInfo}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </shinCodeContext>
 )
-
-export default shinCodeContext
