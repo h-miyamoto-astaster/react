370639844b80bbbc8021daafa24261329c7c99ba
ebans1 <ebans@outlook.jp>
2024-07-17 23:19:05 +0900

    本215ページまで終了

--- react-books/src/App.js
+++ react-books/src/App.js
@@ -1,41 +1,26 @@
 import { useState } from 'react';
-import Counter from './Counter';
-import Hello from './Hello';
 
   const INITIAL_COUNT = 0;
   
   const INITIAL_NAME = "JavaScript";
 
   const SampleComponent = () => {
-    const [count,setCount] = useState(INITIAL_COUNT);
 
-    const [name,setName] = useState(INITIAL_NAME);
+    const [count,setCount] = useState(INITIAL_VALUE);
 
-    const countIncrement = () => setCount((prevCount) => prevCount + 1);
-
-    const countDecrement = () => setCount((prevCount) => prevCount - 1);
-
-    const countReset = () => setCount(INITIAL_COUNT);
-
-    const handleChangeName = (e) =>{
-      setName(e.target.value);
-    } ;
-  
+    const callbackFunction = () =>{
+      document.title = `${count} 回クリックされました`;
+    }
+    useEffect(callbackFunction,[count]);
+    
+    const countIncrement = () =>{
+      setCount((prevCount) => prevCount + 1);
+    };
+    
   return (
     <div className="App">
-      <Counter 
-      count = {count}
-      countIncrement = {countIncrement}
-      countDecrement = {countDecrement}
-      countReset = {countReset}
-      initialCount = {INITIAL_NAME}
-      />
-
-      <Hello 
-      name = {name}
-      handleChangeName = {handleChangeName}
-      initialName = {INITIAL_NAME}
-      />
+      <p>現在のカウント数:{count}</p>
+      
     </div>
   );
 }
