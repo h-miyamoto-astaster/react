58abf549aac8b745edcbfdb1129e1875339c0d1d
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-07-18 14:42:33 +0900

    本220ページまで終了

--- react-books/src/App.js
+++ react-books/src/App.js
@@ -1,34 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-  const INITIAL_COUNT = 0;
+const INITIAL_COUNT = 0;
   
-  const INITIAL_NAME = "JavaScript";
+const Timer = () => {
 
-  const SampleComponent = () => {
+  const [count,setCount] = useState(INITIAL_COUNT);
 
-    const [count,setCount] = useState(INITIAL_VALUE);
+  const countReset = () =>{
+    setCount(INITIAL_COUNT);
+  }
 
-    const callbackFunction = () =>{
-      document.title = `${count} 回クリックされました`;
-    }
-    useEffect(callbackFunction,[count]);
+  const countIncrement = () =>{
+    setCount((prevCount) => prevCount + 1)
+    console.log("カウントアップ+1")
+  }
+
+  const callbackFunction = () =>{
+      alert("副作用関数が実行されました")
     
-    const countIncrement = () =>{
-      setCount((prevCount) => prevCount + 1);
+
+    const timer = setInterval(countIncrement,1000);
+
+    return () =>{
+      console.log("timerが解除されました");
+      clearInterval(timer);
     };
-    
+  }
+  
+  useEffect(callbackFunction,[])
+
   return (
     <div className="App">
       <p>現在のカウント数:{count}</p>
       
+      <button onClick={countReset}>リセット</button>
     </div>
   );
 }
 
 export default function App(){
+  const [display,toggleDisplay] = useState(false);
+
+  const handleToggleDisplay = () =>{
+    toggleDisplay(!display);
+  }
   return(
     <>
-      <SampleComponent />
+      <button onClick={handleToggleDisplay}>
+        {display ? "タイマーを非表示" : "タイマーを表示"}
+      </button>
+
+      {display && <Timer />}
     </>
   )
 };
