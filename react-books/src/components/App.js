e209eb47012e72c5740e7999c9622ad533a8d542
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-07-25 11:17:55 +0900

    json axios他環境構築

--- react-books/src/components/App.js
+++ react-books/src/components/App.js
@@ -1,7 +1,14 @@
 import React,{ useState } from 'react';
+import axios from "axios";
 
 export default function App(){
-
+  const fetchData = async () =>{
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    return console.log(response.data);
+  }
+  fetchData();
   return(
       <>
         <p>これからTodoアプリを実装します。</p>
