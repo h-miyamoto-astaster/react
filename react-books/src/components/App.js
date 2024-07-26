c7076f30853fd2fc1a0c60b66cc23fa00fa689bf
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-07-26 18:20:37 +0900

    コンポーネント化

--- react-books/src/components/App.js
+++ react-books/src/components/App.js
@@ -2,6 +2,33 @@ import React,{ useState,useEffect } from 'react';
 import axios from "axios";
 
 const todoDataUrl = "http://localhost:3100/todos";
+
+const TodoTitle = ({title,as}) =>{
+  if(as == "h1") return <h1>{title}</h1>;
+  if(as == "h2") return <h2>{title}</h2>;
+  return <p>{title}</p>
+}
+
+const TodoItem = ({todo}) =>{
+  return(
+    <li>
+      {todo.content}
+      <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+      <button>削除</button>
+    </li>
+  );
+};
+
+const TodoList = ({todoList}) =>{
+  return (
+    <ul>
+      {todoList.map((todo) =>(
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </ul>
+  )
+}
+
 export default function App(){
   
   const [todoList,setTodoList] = useState([]);
@@ -28,37 +55,16 @@ export default function App(){
 
   return(
       <>
-      <h1>TODO進捗管理</h1>
+        <TodoTitle title="TODO進捗管理" as="h1" />
         <textarea />
           
           <button>+ TODOを追加</button>
 
-          <h2>未完了TODOリスト</h2>
-          <ul>
-            {inCompletedList.map((todo) =>(
-              <li key={todo.id}>
-                {todo.content}
-                <button>
-                  {todo.done ? "未完了リストへ":"完了リストへ"}
-                </button>
+          <TodoTitle title="未完了TODOリスト" as="h2" />
+          <TodoList todolist={inCompletedList} />
+          <TodoTitle title="完了TODOリスト" as="h2" />
+          <TodoList todolist={CompletedList} />
 
-                <button>削除</button>
-              </li>
-            ))}
-          </ul>
-          <h2>TODOリスト</h2>
-          <ul>
-            {todoList.map((todo) =>(
-              <li key={todo.id}>
-                {todo.content}
-                <button>
-                  {todo.done ? "未完了リストへ":"完了リストへ"}
-                </button>
-
-                <button>削除</button>
-              </li>
-            ))}
-          </ul>
       </>
   );
 };
