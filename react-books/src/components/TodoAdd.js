15fb03e3ddbc1806e430a67e1700c60b0f580746
ebans1 <ebans@outlook.jp>
2024-08-01 23:40:19 +0900

    cssの制作を進行

--- react-books/src/components/TodoAdd.js
+++ react-books/src/components/TodoAdd.js
@@ -1,9 +1,26 @@
+import React from 'react'
+import styled from 'styled-components';
+
 export const TodoAdd = ({buttonText,inputEl,handleAddTodoListItem}) =>{
-    return(
-      <>
-        <textarea ref={inputEl}/>
+  const Btn02 = styled.button`
+    background-color:white;
+    width:300px;
+    height:50px;
+    border-radius:25px;
+    border:1px solid dimgray;
+    text-align:center;
+  `;  
   
-        <button onClick={handleAddTodoListItem}>{buttonText}</button>
+  const Textarea1 = styled.textarea`
+    width:100%;
+    max-width:500px;
+    padding:20px;
+  `;
+  return(
+      <>
+        <Textarea1 ref={inputEl}/>
+  
+        <Btn02 onClick={handleAddTodoListItem}>{buttonText}</Btn02>
       </>
     );
   };
