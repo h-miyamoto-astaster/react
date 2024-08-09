dfe616b560028113320d64d9f01d4055f4fe6be5
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-08-09 17:36:17 +0900

    yarnでビルド再試行

--- react-books/src/App.js
+++ react-books/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState,useEffect } from "react";
 
+
+const todoDataUrl = "http://localhost:3100/todos"
 function App() {
+
+  const [todoList,setTodoList] = useState([]);
+  useEffect(() => {
+    const fetchData = async() =>{
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  },[]);
+
+  console.log("Todoリスト",todoList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>これから掲示板アプリを実装します。</p>
     </div>
   );
 }
