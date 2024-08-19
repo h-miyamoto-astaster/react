a634e016b50375baec1272189845cf2155aae7de
ebans1 <ebans@outlook.jp>
2024-08-19 22:51:40 +0900

    書き込み制限の追加

--- react-books/src/App.js
+++ react-books/src/App.js
@@ -63,7 +63,12 @@ function App() {
   const nameEl = useRef(null);
 
   const handleAddBoardListItem = () =>{
+    let writeLen = 10;
+    if(writeLen > 7) {
+      document.querySelector("#writeAlert").textContent("テスト");
+    };
     if(inputEl.current.value === "") return;
+
     if(nameEl.current.value === "") {
       nameEl.current.value = "名無しさん"
     };
@@ -74,14 +79,13 @@ function App() {
 
   const Btn01 = styled.button`
     display:block;
-    width:300px;
+    width:250px;
     height:35px;
     line-height:35px;
     margin:auto;
-    margin-top:50px;
+    margin-top:20px;
     padding:0px 15px;
     font-size:16px;
-    font-weight:bold;
     cursor:pointer;
   `;
 
@@ -91,10 +95,11 @@ function App() {
 
   const NameTextarea = styled.input`
     display:block;
-    width:98%;
+    width:100%;
     padding:5px;
     border:1px solid dimgray;
     border-radius:5px 5px 0px 0px;
+    box-sizing:border-box;
     &:placeholder{
       color:silver;
     }
@@ -103,27 +108,32 @@ function App() {
   const WriteTextarea = styled.textarea`
     display:block;
     width:100%;
+    height:200px;
+    box-sizing:border-box;
   `;
   return (
     <>
       <div className="boardWrap">
-        <WriteTitle title="React掲示板" as="h1"/>
+        <WriteTitle className="mainTitle" title="React掲示板" as="h1"/>
 
-        <WriteTitle title="書き込み" as="h2"/>
+        <div className="boardArea">
 
-        <div>
-          {boardList.map((write) => (
-            <BoardItem write={write} key={write.id} deleteBoardListItem={deleteBoardListItem}/>
-          ))}
+          <div>
+            {boardList.map((write) => (
+              <BoardItem write={write} key={write.id} deleteBoardListItem={deleteBoardListItem}/>
+            ))}
+          </div>
+          
+          <WriteArea>
+            <WriteTitle title="書き込み" as="h2"/>
+            <NameTextarea ref={nameEl} placeholder="名前"/>
+
+            <WriteTextarea ref={inputEl} />
+
+            <Btn01 onClick={handleAddBoardListItem}>書き込む</Btn01>
+            <p id="writeAlert"></p>
+          </WriteArea>
         </div>
-        
-        <WriteArea>
-          <NameTextarea ref={nameEl} placeholder="名前"/>
-
-          <WriteTextarea ref={inputEl} />
-
-          <Btn01 onClick={handleAddBoardListItem}>書き込む</Btn01>
-        </WriteArea>
       </div>
     </>
   );
