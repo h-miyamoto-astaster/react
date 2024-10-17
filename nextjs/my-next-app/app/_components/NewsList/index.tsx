e7c33c5effb0c3775dc6920026638bc1495f92d7
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-10-17 14:48:56 +0900

    appを作成

---
+++ nextjs/my-next-app/app/_components/NewsList/index.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+
+type News = {
+  id:string;
+  title:string;
+  category:{
+    name:string;
+  };
+  publishedAt:string;
+  createdAt:string;
+};
+type Props ={
+    news:News[];
+};
+
+export default function NewsList({news}:Props) {
+
+  return (
+
+    <ul>
+      {news.map((article) =>(
+        <li key={article.id}  className={styles.list}>
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="NO Image"
+              width={1200}
+              height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.newsItemTitle}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <span className={styles.tag}>{article.category.name}</span>
+                <span className={styles.data}>
+                  <Image
+                    src="/clock.svg"
+                    alt=""
+                    width={16}
+                    height={16}
+                    priority
+                  />
+                  {article.publishedAt}
+                </span>
+              </dd>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+
+ );
+}
