0d994cfe2457db2f8bb9df75fe36e641370daeb3
ebans1 <ebans@outlook.jp>
2024-10-21 00:18:51 +0900

    第3章まで終了

---
+++ nextjs/my-next-app/app/_components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
+export default function Header(){
+   return(
+      <header className={styles.header}>
+         <a href="/" className={styles.logo}>
+         <Image
+         className={styles.logo}
+         src="/logo.svg"
+         alt="SIMPLE"
+         width={348}
+         height={133}
+         priority
+         />
+      </a>
+      </header>
+   )
+}
