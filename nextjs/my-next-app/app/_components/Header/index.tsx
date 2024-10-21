7704152c17a35fa20c1f0562d2a94895d16a13aa
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-10-21 14:55:58 +0900

    ルーティング関連

--- nextjs/my-next-app/app/_components/Header/index.tsx
+++ nextjs/my-next-app/app/_components/Header/index.tsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 export default function Header(){
    return(
       <header className={styles.header}>
-         <a href="/" className={styles.logo}>
-         <Image
-         className={styles.logo}
-         src="/logo.svg"
-         alt="SIMPLE"
-         width={348}
-         height={133}
-         priority
-         />
-      </a>
+         <Link href="/" className={styles.logo}>
+            <Image
+               className={styles.logo}
+               src="/logo.svg"
+               alt="SIMPLE"
+               width={348}
+               height={133}
+               priority
+            />
+         </Link>
+      <nav>
+         <ul className={styles.item}>
+            <li>
+               <Link href="/members">メンバー</Link>
+            </li>
+         </ul>
+      </nav>
       </header>
    )
 }
