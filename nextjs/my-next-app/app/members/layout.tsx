c08783ebb3a6c71059b205e1b258bb5f4c420a7a
ebans1 <ebans@outlook.jp>
2024-10-23 23:17:24 +0900

    制作を進行

---
+++ nextjs/my-next-app/app/members/layout.tsx
@@ -0,0 +1,11 @@
+import Sheet from "@/app/_components/Sheet";
+
+type Props = {
+   children:React.ReactNode;
+};
+
+export default function RootLayout({ children }:Props){
+   return (
+      <Sheet>{children}</Sheet>
+   )
+}
