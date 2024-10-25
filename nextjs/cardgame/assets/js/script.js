85e3ceda24f8ca1efb82b0a05b1f559cb1508851
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-10-25 18:37:36 +0900

    カードゲームを追加

---
+++ nextjs/cardgame/assets/js/script.js
@@ -0,0 +1,7 @@
+function init(){
+    document.getElementById("enemyarea");
+    let cardElement = document.createElement("div");
+    cardElement.className = "card";
+    cardElement.textsContent = "テスト";
+    enemyarea.appendChild(cardElement);
+}
