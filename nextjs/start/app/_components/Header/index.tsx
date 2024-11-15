9f4ad90b14c20cf444ea6948fc742c8cff873493
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-11-15 23:52:14 +0900

    jsを記述

--- nextjs/start/app/_components/Header/index.tsx
+++ nextjs/start/app/_components/Header/index.tsx
@@ -1,13 +1,27 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 
-let btnClick = () =>{
-   console.log("ok");
-};
-
 export default function Header(){
 
+   let btnClick = () =>{
    
+      console.log("ok");
+      let header_btn = document.getElementById("header__btn");
+      let header_spmenu = document.getElementById("header-spmenu");
+      let openBtn1 = document.querySelector("#header__btn #openbtn1");
+      header_btn.addEventListener('click',function(){
+         if(header_btn.classList.contains("active")){
+            openBtn1.classList.remove("active");
+            header_btn.classList.remove("active");
+            header_spmenu.classList.remove("active");
+         }else{
+            openBtn1.classList.add("active");
+            header_btn.classList.add("active");
+            header_spmenu.classList.add("active");
+         }
+      });
+   };
+
    return(
       <header className="header">
          <div className="header__container">
@@ -25,14 +39,14 @@ export default function Header(){
                <li className="header__menu"><a href="./about/index.html">会社概要</a></li>
             </ul>
          </div>
-         <ul className="header-spmenu">
+         <ul id="header-spmenu" className="header-spmenu">
             <div className="header-spmenu__inner">
                <li className="header-spmenu__item"><a href="#">ホーム</a></li>
                <li className="header-spmenu__item"><a href="./message/index.html">メッセージ</a></li>
                <li className="header-spmenu__item"><a href="./about/index.html">会社概要</a></li>
             </div>
          </ul>
-         <div className="header__btn" onClick="btnClick">
+         <div id="header__btn" className="header__btn" onClick={btnClick}>
             <div className="openbtn1"><span></span><span></span><span></span></div>
          </div>
       </header>
