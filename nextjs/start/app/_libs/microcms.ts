1c47fc92cca580202928ec2dc21683f3067efb27
Lenovo-ThinkPad\h-miyamoto <h-miyamoto@astaster.jp>
2024-11-20 18:03:30 +0900

    newsページを復活

--- nextjs/start/app/_libs/microcms.ts
+++ nextjs/start/app/_libs/microcms.ts
@@ -1,11 +1,48 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+    MicroCMSQueries,
+    MicroCMSImage,
+    MicroCMSListContent,
+} from "microcms-js-sdk";
+
 export type News = {
     id:string;
     title:string;
+    content:string;
+    image:MicroCMSImage;
     publishedAt:string;
 }
+
+/*export type News = {
+    id:string;
+    title:string;
+    publishedAt:string;
+}*/
 export type Title = {
     title:string;
 }
 export type Root_path = {
     root_path:string;
+}
+
+if(!process.env.MICROCMS_SERVICE_DOMAIN){
+    throw new Error("MICROCMS_SERVICE_DOMAIN is requierd");
+}
+
+if(!process.env.MICROCMS_API_KEY){
+    throw new Error("MICROCMS_API_KEY is requierd");
+}
+
+const client = createClient({
+    serviceDomain:process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey:process.env.MICROCMS_API_KEY,
+});
+
+export const getNewsList = async(queries?:MicroCMSQueries) =>{
+    const listData = await client
+    .getList<News>({
+        endpoint:"news",
+        queries,
+    });
+    return listData;
 }
---
+++ nextjs/start/app/news/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Title } from "@/app/_libs/microcms"
+
+import Header from "@/app/_components/Header";
+import Footer from "@/app/_components/Footer";
+import Mvsingle from "@/app/_components/Mvsingle";
+
+export default function Page(){
+    return(
+        <>
+            <Header root_path={"../"}/>
+
+            <Mvsingle title={"ニュース"}/>
+            <section className="newspage">
+                <div className="newspage__inner">
+                    <h2 className="newspage__title">コーポレートサイトをリニューアルしました。</h2>
+                    <div className="newspage__img">
+                        <Image
+                            className="nextjsimage"
+                            src="/img/news/news-img01.jpg"
+                            alt=""
+                            fill
+                        />
+                    </div>
+                    <p className="newspage__text">
+                        平素より格別のご愛顧を賜り、誠にありがとうございます。<br />
+                        このたび、当社はコーポレートサイトを全面的にリニューアルいたしました。<br />
+                        <br />
+                        新しいサイトでは、より一層使いやすく、視覚的に魅力的なデザインを採用し、企業情報やサービス内容の紹介をさらに充実させました。また、モバイル端末にも最適化され、どのデバイスからでも快適にご利用いただけるよう改善いたしました。<br />
+                        <br />
+                        今後とも、より一層の情報提供を行い、皆様にとって有益なコンテンツを発信してまいりますので、ぜひご覧ください。<br />
+                        <br />
+                        今後とも変わらぬご支援を賜りますようお願い申し上げます。
+                    </p>
+                </div>
+            </section>
+
+            <Footer root_path={"../"}/>
+        </>
+    )
+}
